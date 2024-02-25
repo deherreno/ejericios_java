d84c7e858f21236e8a6734d96fb66721feb9c1f2
Daniel <deherreo@soy.sena.edu.co>
2024-02-24 20:57:15 -0500

    segundo commit suma de numeros

---
+++ src/main/java/co/edu/sena/App1.java
@@ -0,0 +1,19 @@
+package co.edu.sena;
+
+import java.util.Scanner;
+
+public class App1 {
+    public static void main( String[] args )
+    {
+        System.out.println("Suma de 2 numeros");
+        System.out.println("Ingrese el primer numero");
+        Scanner src= new Scanner (System.in);
+        double num1 = src.nextDouble();
+        System.out.println("Ingrese el segundo numero");
+        double num2 = src.nextDouble();
+        double result = (num1 + num2);
+        System.out.println("La suma de los 2 numeros es: " + result);
+    }
+}
+
+
