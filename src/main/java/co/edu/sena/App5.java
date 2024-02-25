022abc6e6bff15c25f97e293e65755a3a9e3d156
Daniel <deherreo@soy.sena.edu.co>
2024-02-24 23:19:44 -0500

    sexto commit area y volumen de un cilindro

---
+++ src/main/java/co/edu/sena/App5.java
@@ -0,0 +1,18 @@
+package co.edu.sena;
+
+import java.util.Scanner;
+
+public class App5 {
+     public static void main(String[] args) {
+              
+         System.out.println("Area y volumen de un cilindro");         
+         Scanner cilindro = new Scanner(System.in);           
+         System.out.println("Ingrese la radio del cilindro");         
+         double radio = cilindro.nextDouble();           
+         System.out.println("Ingrese la altura del cilindro");         
+         double altura = cilindro.nextDouble();           
+         double area = 2 * Math.PI * radio *(radio + altura ) ;         
+        double volumen= Math.PI * Math.pow(radio,2) * altura;
+         System.out.println("El area del cilindro es " +area+"y el volumen del cilindro es: "+volumen );         
+    }
+}
